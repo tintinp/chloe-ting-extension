bb604421970b402fee801955197ba6c40675e745
tintinp <tinpatana@gmail.com>
2020-07-24 22:46:00 -0700

    collect data

--- data-collection-chrome-extension/src/js/Managers/DataManager.js
+++ data-collection-chrome-extension/src/js/Managers/DataManager.js
@@ -1,42 +1,47 @@
-import { setActiveSignal, updateCount } from '../redux/actions'
+import { abs, floor, log, mean, pow, round } from 'mathjs'
+import { addLog, setActiveSignal, updateCount } from '../redux/actions'
 
 import CONSTANTS from '../constants/CONSTANTS'
 import Meyda from 'meyda'
-import { addLog } from '../redux/actions'
-import eventManager from './EventManager'
 import isNumber from '../Utils/isNumber'
-import { mean } from 'lodash'
 
+// Audio context of the browser
 const AudioContext = window.AudioContext || window.webkitAudioContext
+
+// Features to calculate additional delta between current and previous values
+const FEATURES_SINGLE_VALUE = [
+  'rms',
+  'energy',
+  'spectralCentroid',
+  'spectralFlatness',
+  'spectralRolloff',
+  'spectralSkewness',
+  'perceptualSpread',
+  'perceptualSharpness'
+]
+
+const FEATURES_ARRAY = ['mfcc', 'powerSpectrum']
+
+const ALL_FEATURES = FEATURES_SINGLE_VALUE.concat(FEATURES_ARRAY)
 
 class DataManager {
   constructor(options) {
-    this.analyzer = null
-    this.running = false
     this.store = options.store
-    this.eventManager = eventManager
+    this.running = false
+    this.analyzer = null
     this.selectedClass = null
+    this.currentAudioContext = null
     this.sampleLength = CONSTANTS.DEFAULT_SAMPLE_LENGTH
+    this.bufferSize = null
+    this.nFrame = 0
+    this.targetNFrame = null
     this.count = {
       chloe: 0,
       music: 0
     }
-    this.currentAudioContext = null
-    this.nFFTCount = 0
-    this.targetNFFT = null
-    this.dataset = []
-    this.dataHolder = {
-      rms: [],
-      powerSpectrum: [],
-      mfcc: [],
-      spectralCentroid: [],
-      spectralFlux: []
-    }
-    this.init()
-  }
-
-  init() {
-    eventManager.on(CONSTANTS.EVENTS.SELECTED_CLASS_CHANGE, () => {})
+    this.data = this.getEmptyDataHolder()
+    this.delta = this.getEmptyDataHolder()
+    this.dataInCurrentFrame = this.getEmptyDataHolder()
   }
 
   // --------------------------------------------------------------------------
@@ -65,8 +70,9 @@ class DataManager {
     this.audioElement = null
     this.currentAudioContext = null
     this.activeSignal = false
-    this.nFFTCount = 0
-    this.targetNFFT = null
+    this.bufferSize = null
+    this.nFrame = 0
+    this.targetNFrame = null
     this.store.dispatch(setActiveSignal(false))
     this.store.dispatch(addLog('Stop collecting data'))
   }
@@ -81,6 +87,16 @@ class DataManager {
     this.sampleLength = sampleLength
   }
 
+  export() {
+    // Remove data that might be due to delay in human speed switching class in the UI
+
+    // Export to CSV
+
+    // Reset data and count
+    this.reset()
+    console.log('Exported data to CSV')
+  }
+
   // --------------------------------------------------------------------------
   // Private Functions
   // --------------------------------------------------------------------------
@@ -89,8 +105,9 @@ class DataManager {
     return Meyda.createMeydaAnalyzer({
       audioContext: this.currentAudioContext,
       source: source,
-      bufferSize: CONSTANTS.BUFFER_SIZE,
-      featureExtractors: ['rms', 'spectralFlatness'],
+      bufferSize: this.bufferSize,
+      numberOfMFCCCoefficients: CONSTANTS.N_MFCC,
+      featureExtractors: ALL_FEATURES,
       callback: (features) => {
         this.extractFeatures(features)
       }
@@ -98,35 +115,110 @@ class DataManager {
   }
 
   extractFeatures(features) {
-    const { rms, spectralFlatness } = features
-    if (this.nFFTCount < this.targetNFFT) {
-      this.nFFTCount += 1
-      if (isNumber(spectralFlatness)) {
-        this.dataHolder.rms.push(spectralFlatness)
+    // Discard when any of the features have invalid value
+    if (this.isValidValue(features)) {
+      if (this.nFrame < this.targetNFrame) {
+        // Accumulate features in current time frame
+        ALL_FEATURES.forEach((featureName) => {
+          const value = features[featureName]
+          this.dataInCurrentFrame[featureName].push(value)
+        })
+
+        // Increment frame
+        this.nFrame += 1
+      } else {
+        // If class is selected
+        if (this.selectedClass) {
+          // Get mean of values in current frame along with delta between each sample in the frame
+          FEATURES_SINGLE_VALUE.forEach((featureName) => {
+            const dataInCurrentFrame = this.dataInCurrentFrame[featureName]
+            const data = this.data[featureName]
+            const delta = this.delta[featureName]
+            const arrLength = data.length
+
+            if (dataInCurrentFrame.length > 0) {
+              const avg = mean(dataInCurrentFrame)
+              const d1 = arrLength < 1 ? null : abs(avg - data[arrLength - 1])
+              const d2 = arrLength < 2 ? null : abs(avg - data[arrLength - 2])
+
+              if (d1 !== null) {
+                delta.push(Math.max(d1, d2))
+              } else {
+                delta.push(NaN)
+              }
+              data.push(avg)
+            } else {
+              data.push(NaN)
+              delta.push(NaN)
+            }
+          })
+
+          // Get mean MFCC in the current frame, mean along axis 0
+          this.data.mfcc.push(mean(this.dataInCurrentFrame.mfcc, 0))
+
+          // Get mean power spectrum in the current frame, mean along axis 0
+          this.data.powerSpectrum.push(
+            mean(
+              // Convert Float32Array into normal array
+              this.dataInCurrentFrame.powerSpectrum.map((arr) => Array.from(arr)),
+              0
+            )
+          )
+
+          // Get class of audio of current frame
+          this.data.class.push(this.getClass())
+
+          // Update class count
+          this.incrementDatasetCount()
+        }
+
+        // Reset current frame data
+        this.dataInCurrentFrame = this.getEmptyDataHolder()
+
+        // Reset number of samples needed for time frame
+        this.nFrame = 0
+
+        // Update signal status
+        this.checkActiveSignal(features.rms)
+        console.log(this.data)
       }
-      // console.log(perceptualSpread)
-    } else {
-      this.nFFTCount = 0
-      console.log(mean(this.dataHolder.rms))
-      this.dataHolder.rms = []
-      this.incrementDatasetCount()
-      this.checkActiveSignal(rms)
     }
   }
 
   calculateDataNeededPerSampleLength() {
     // sampleRate = sampling rate of WebAPI audio (usually 48000)
-    // BUFFER_SIZE = number of samples in each FFT calculation
-    // frameDuration = number of milliseconds of sample per each FFT calculation
-    // targetNFFT = number of FFT calculation needed to reach duration of sampleLength
+    // bufferSizePowOf2 = number of audio data in a buffer for each FFT calculation
+    // frameLength = number of milliseconds of sample per each FFT calculation
+    // targetNFrame = number of FFT calculation needed to reach duration of sampleLength
     // sampleLength = number of milliseconds of sample per each dataset (to be used in ML training)
+    // TARGET_FRAME_LENGTH = Default to 25ms of audio for each FFT calculation
+
+    // Need to calculate buffer size (as power of 2) in order for ~25ms of audio to be used in
+    // calculating FFT and other featuers. This will depend on the sample rate of the browser.
+
+    // Chrome sample rate is usually 48000 samples per second
+    // Therefore, buffer size should be 0.025 * 48000 = 1200
+    // Which this is the size of the buffer to give 25ms of audio
+
+    // However, 1024 is the closest power of two
+    // Hence, we'll use buffer size of 1024
+
+    // We get 48000 samples per second, so to get 1024 samples, it will take 21.333ms
+    // This means every 21.333ms we will get 1 audio feature extraction
+    // Hence, to get ~700ms worth of sample, we have to collect ~32 extractions
+    // Therefore, we collect 32 extractions, then find the mean of each feature
+    // The mean for each feature will count as 1 dataset
 
     const sampleRate = this.currentAudioContext.sampleRate
-    const frameDuration = (CONSTANTS.BUFFER_SIZE / sampleRate) * 1000.0
-    this.targetNFFT = this.sampleLength / frameDuration
+    const theoreticalBufferSize = CONSTANTS.TARGET_FRAME_LENGTH * sampleRate
+    const bufferSizePowOf2 = pow(2, round(log(theoreticalBufferSize, 2)))
+    const frameLength = (bufferSizePowOf2 / sampleRate) * 1000.0
+    this.bufferSize = bufferSizePowOf2
+    this.targetNFrame = floor(this.sampleLength / frameLength)
     console.log(`Sampling rate: ${sampleRate} Hz`)
-    console.log(`Frame duration: ${frameDuration} ms`)
-    console.log(`Number of FFT per dataset: ${this.targetNFFT}`)
+    console.log(`Frame duration: ${frameLength} ms`)
+    console.log(`Buffer size needed: ${bufferSizePowOf2}`)
+    console.log(`Number of FFT per dataset: ${this.targetNFrame}`)
   }
 
   checkActiveSignal(rms) {
@@ -161,6 +253,41 @@ class DataManager {
     }
     this.store.dispatch(updateCount(CONSTANTS.CHLOE, this.count.chloe))
     this.store.dispatch(updateCount(CONSTANTS.MUSIC, this.count.music))
+  }
+
+  getEmptyDataHolder() {
+    return {
+      rms: [],
+      energy: [],
+      spectralCentroid: [],
+      spectralFlatness: [],
+      spectralRolloff: [],
+      spectralSkewness: [],
+      perceptualSpread: [],
+      perceptualSharpness: [],
+      mfcc: [],
+      powerSpectrum: [],
+      class: []
+    }
+  }
+
+  isValidValue(features) {
+    return FEATURES_SINGLE_VALUE.reduce((isValid, featureName) => {
+      return isValid && isNumber(features[featureName])
+    }, true)
+  }
+
+  getClass() {
+    return CONSTANTS.CLASS_TO_NUMBER[this.selectedClass]
+  }
+
+  reset() {
+    this.data = this.getEmptyDataHolder()
+    this.delta = this.getEmptyDataHolder()
+    this.dataInCurrentFrame = this.getEmptyDataHolder()
+    this.count.chloe = 0
+    this.count.music = 0
+    this.nFrame = 0
   }
 }
 
