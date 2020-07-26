27eebf010a37ddf34c69ddd56ac41b3949caae06
tintinp <tinpatana@gmail.com>
2020-07-25 23:15:41 -0700

    start on data analysis notebook

---
+++ machine-learning/CONSTANTS.py
@@ -0,0 +1,38 @@
+CLASS_NAMES = {
+    'CHLOE': 'CHLOE',
+    'BEEP': 'BEEP',
+    'MUSIC': 'MUSIC'
+}
+
+INT_TO_CLASS = {
+    'CHLOE': '0',
+    'BEEP': '1',
+    'MUSIC': '2'
+}
+
+CLASS_TO_INT = {
+    0: 'CHLOE',
+    1: 'BEEP',
+    2: 'MUSIC'
+}
+
+FEATURES = {
+    'RMS': 'rms',
+    'DELTA_RMS': 'delta_rms',
+    'ENERGY': 'energy',
+    'DELTA_ENERGY': 'delta_energy',
+    'SPECTRAL_CENTROID': 'spectralCentroid',
+    'DELTA_SPECTRAL_CENTROID': 'delta_spectralCentroid',
+    'SPECTRAL_FLATNESS': 'spectralFlatness',
+    'DELTA_SPECTRAL_FLATNESS': 'delta_spectralFlatness',
+    'SPECTRAL_ROLLOFF': 'spectralRolloff',
+    'DELTA_SPECTRAL_ROLLOFF': 'delta_spectralRolloff',
+    'SPECTRAL_SKEWNESS': 'spectralSkewness',
+    'DELTA_SPECTRAL_SKEWNESS': 'delta_spectralSkewness',
+    'PERCEPTUAL_SPREAD': 'perceptualSpread',
+    'DELTA_PERCEPTUAL_SPREAD': 'delta_perceptualSpread',
+    'PERCEPTUAL_SHARPNESS': 'perceptualSharpness',
+    'DELTA_PERCEPTUAL_SHARPNESS': 'delta_perceptualSharpness',
+    'MFCC': 'mfcc',
+    'POWER_SPECTRUM': 'powerSpectrum'
+}
